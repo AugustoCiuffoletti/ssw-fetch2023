47f87b58bb2114a0dcedc39126aa90058cd02c4c
AugustoCiuffoletti <augusto.ciuffoletti@unipi.it>
2023-03-21 11:12:12 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 // Import stylesheets
 import './style.css';
-const key = '3bfb3112';
 const base =
   'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint';
 const op = 'get';
+const key = '3bfb3112';
 
 const URL = base + '/' + op + '?key=' + key;
 console.log(URL);
 fetch(URL)
   .then(
-    (response) => {
-      console.log(response);
-      return response.json()
-    },
+    (response) => response.json(),
     (error) => alert(error)
   )
   .then((data) => {
+    console.log(data);
     const db = JSON.parse(data);
-    console.log(db[0].autore);
+    console.log(db);
   });
